0a49ec9ef69b50bea67cb5c2fefcb888bf866c65
Philip Maechling <pjmaechling@users.noreply.github.com>
2024-02-24 00:35:38 -0800

    Working Dockerfile on Mac

--- use_case_1/build_ucerf3-etas.sh
+++ use_case_1/build_ucerf3-etas.sh
@@ -1,1 +1,1 @@
-docker build --progress=plain --no-cache=false -f Dockerfile . -t ucerf3-etas
+docker build --progress=plain --no-cache=false -f Dockerfile . -t ucerf3_jup
--- use_case_1/run_ucerf3_jup.sh
+++ use_case_1/run_ucerf3_jup.sh
@@ -1,5 +1,5 @@
 # -rm removes container on exit
 # -it indicates inteactive + --tty so that users are at an interactive tty command line when the container starts
 
-mkdir -p target
-docker run -i -t ucerf3_jup
+#mkdir -p target
+docker run -p 8888:8888 --rm -it --mount type=bind,source="$(pwd)"/target,destination=/home/scecuser/target ucerf3_jup:latest
